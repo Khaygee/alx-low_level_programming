8949aca4686d4c852edb5395c3e88d22ceb3bcb9
Khaygee <Cakinfemiwa@gmail.com>
2022-09-19 13:06:42 -0700

    prints a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _puts - Print a string
+ *  Auth: Cyntha K
+ *  @str: Set of characters
+ *
+ *  Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
